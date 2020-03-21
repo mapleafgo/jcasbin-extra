a4cf781c61bbb0297de8cc99e228e3e6df502d28
慕枫java <javakang@qq.com>
2020-03-21 21:59:07 +0800

    配置发布文件

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     `java-library`
     `maven-publish`
+    signing
     id("io.freefair.lombok") version "4.1.6"
 }
 
@@ -33,14 +34,54 @@ tasks.withType<Javadoc>() {
     options.encoding = "utf-8"
 }
 
+val NEXUS_USERNAME: String by project;
+val NEXUS_PASSWORD: String by project;
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
             from(components["java"])
             pom {
-                name.set("jcasbin extra")
+                url.set("https://github.com/fanlide/jcasbin-extra")
+                name.set(project.name)
                 description.set("JCasbin 的扩充，包含 HutoolDB Adapter，Etcd Watcher")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("./LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mufeng")
+                        name.set("慕枫")
+                        email.set("javakang@qq.com")
+                    }
+                }
+                scm {
+                    connection.set("https://github.com/fanlide/jcasbin-extra.git")
+                    url.set(pom.url)
+                }
+            }
+        }
+        repositories {
+            mavenLocal {
+                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                credentials {
+                    username = NEXUS_USERNAME
+                    password = NEXUS_PASSWORD
+                }
+            }
+            mavenCentral {
+                url = uri("https://oss.sonatype.org/content/repositories/snapshots")
             }
         }
     }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
